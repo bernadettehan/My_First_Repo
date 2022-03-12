0f2b7792c517237d762ed0e3653b216a96fdfa07
bernadettehan <86629591+bernadettehan@users.noreply.github.com>
2022-03-12 04:20:31 -0500

    HW19 Complete

---
+++ MyCode/UX_UI_HW_19/js/index.js
@@ -0,0 +1,19 @@
+console.log("Your index.js file is loaded correctly!");
+
+function mouseOver(x) {
+    x.style.color = "#A7341C";
+   }
+   
+function mouseOut(x) {
+     x.style.color = "#D853A2";
+}
+
+function zoomImg(x) {
+    x.style.height = "350px";
+    x.style.width = "350px";
+}
+  
+function normalImg(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+}
